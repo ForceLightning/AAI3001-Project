9398be294c9de4b896e7871a246c991ba6c04eff
Christopher Kok <ForceLightning@users.noreply.github.com>
2023-11-30 23:44:46 +0800

    update foolbox whitebox eval

--- foolbox_whitebox_eval.py
+++ foolbox_whitebox_eval.py
@@ -1,28 +1,29 @@
+"""
+Runs the whitebox evaluation on the test set.
+"""
 import csv
 import gc
 import os
-import sys
-from typing import List
+from typing import List, Tuple
 
-import fastai
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
+import seaborn as sns
 import torch
-import torch.nn as nn
-import torchvision
 import torchvision.transforms.functional as F
 from fastai.callback.all import MixedPrecision
 from fastai.data.all import DataLoaders
 from fastai.learner import CastToTensor
-from fastai.vision.all import (BCEWithLogitsLossFlat, DataLoader,
-                               SegmentationDataLoaders, resnet50, unet_learner)
+from fastai.vision.all import DataLoader, resnet50
 from fastai.vision.learner import Learner, create_unet_model
 from PIL import Image
+from torch import nn
 from torchvision.transforms import v2
 from torchvision.utils import draw_segmentation_masks
 from tqdm.auto import tqdm
 
-from utils.dataset import MoNuSegDataset, MultiEpochsDataLoader
+from utils.dataset import MoNuSegDataset
 from utils.lossmetrics import BinaryDice, CombinedBCEDiceLoss, PixelAccuracy
 
 VALID_DIR = os.path.join(
@@ -38,7 +39,23 @@ DEVICE = torch.device("cuda:0" if USE_CUDA else "cpu")
 EPSILONS = [0.0, 1e-2, 1.5e-2, 3e-2, 6e-2, 1e-1, 3e-1]
 
 
-def get_fold_adversarial_img(output_dir, fold_idx, img_idx, eps_idx):
+def get_fold_adversarial_img(
+    output_dir: str | os.PathLike,
+    fold_idx: int,
+    img_idx: int,
+    eps_idx: int
+) -> str | os.PathLike:
+    """Gets the path to the adversarial image for a given fold, image, and epsilon.
+
+    Args:
+        output_dir (str): The output directory.
+        fold_idx (int): The fold index.
+        img_idx (int): The image index.
+        eps_idx (int): The epsilon index.
+
+    Returns:
+        str: The path to the adversarial image.
+    """
     return os.path.join(output_dir, f"fold_{fold_idx}", f"img_{img_idx}_eps_{eps_idx}.png")
 
 
@@ -46,8 +63,20 @@ def show(
     imgs: List[torch.Tensor] | torch.Tensor,
     labels: List[str],
     title: str = "",
-    show: bool = True
-):
+    show_fig: bool = True
+) -> None | Tuple[plt.Figure, plt.Axes]:
+    """Shows a list of images.
+
+    Args:
+        imgs (List[torch.Tensor] | torch.Tensor): Image or list of images.
+        labels (List[str]): List of labels.
+        title (str, optional): Title string. Defaults to "".
+        show_fig (bool, optional): If true, shows the figure.
+            Disable when many calls are made to this function. Defaults to True.
+
+    Returns:
+        None | Tuple[plt.Figure, plt.Axes]: If show_fig is False, returns the figure and axes.
+    """
     if not isinstance(imgs, list):
         imgs = [imgs]
     fig, axs = plt.subplots(ncols=len(imgs), squeeze=False)
@@ -58,9 +87,10 @@ def show(
         axs[0, i].imshow(np.asarray(img))
         axs[0, i].set(xticklabels=[], yticklabels=[], xticks=[], yticks=[])
         axs[0, i].set_title(labels[i])
-    plt.suptitle(title)
-    if show:
+    fig.suptitle(title)
+    if show_fig:
         plt.show()
+        return
     return fig, axs
 
 
@@ -80,10 +110,10 @@ def main():
                                 pin_memory=True, sampler=None, shuffle=False, persistent_workers=True,
                                 device=DEVICE)
 
-    with open(os.path.join(OUTPUT_DIR, "whitebox_test.csv"), "a") as f:
+    with open(os.path.join(OUTPUT_DIR, "whitebox_test.csv"), "w") as f:
         writer = csv.writer(f)
         writer.writerow(["fold", "img", "eps", "acc", "dice"])
-        for fold_num in range(NUM_FOLDS):
+        for fold_num in tqdm(range(NUM_FOLDS), desc="Folds"):
             model = create_unet_model(
                 resnet50, n_out=1, img_size=(256, 256), pretrained=False
             )
@@ -119,13 +149,37 @@ def main():
                     )
                     images_and_masks.append(image_and_mask)
                     print(
-                        f"Fold {fold_num}, img {img_idx}, eps {eps:.2e},acc {acc.value * 100:4.2f}%, dice {dice:.4f}")
+                        f"Fold {fold_num}, img {img_idx}, eps {eps:.2e}, acc {acc.value * 100:4.2f}%, dice {dice:.4f}")
                     writer.writerow([fold_num, img_idx, eps, acc.value, dice])
 
                 fig, _ = show(images_and_masks, [r"$\varepsilon = %.2e$" % (
-                    eps) for eps in EPSILONS], title=f"Fold {fold_num}, img {img_idx}", show=False)
+                    eps) for eps in EPSILONS], title=f"Fold {fold_num}, img {img_idx}", show_fig=False)
                 fig.savefig(os.path.join(
                     OUTPUT_DIR, f"fold_{fold_num}_img_{img_idx}.png"))
+                plt.close()
+
+    gc.collect()
+    with torch.no_grad():
+        torch.cuda.empty_cache()
+
+    sns.set("paper", style="whitegrid")
+
+    df = pd.read_csv(os.path.join(OUTPUT_DIR, "whitebox_test.csv"), header=0)
+    ax = sns.lineplot(data=df, x="eps", y="acc", hue="fold", legend="full")
+    ax.set_xlabel(r"$L^\infty$ perturbation $\varepsilon$")
+    ax.set_ylabel("Accuracy")
+    ax.set_title(r"Accuracy vs. $L^\infty$ perturbation")
+
+    plt.savefig(os.path.join(OUTPUT_DIR, "whitebox_test_acc.png"))
+    plt.show()
+
+    ax = sns.lineplot(data=df, x="eps", y="dice", hue="fold", legend="full")
+    ax.set_xlabel(r"$L^\infty$ perturbation $\varepsilon$")
+    ax.set_ylabel("Dice score")
+    ax.set_title(r"Dice score vs. $L^\infty$ perturbation")
+
+    plt.savefig(os.path.join(OUTPUT_DIR, "whitebox_test_dice.png"))
+    plt.show()
 
 
 if __name__ == "__main__":
