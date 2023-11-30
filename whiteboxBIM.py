3b16607a002ef4f2e78fa7b0cfa1fee35787ab12
Christopher Kok <ForceLightning@users.noreply.github.com>
2023-12-01 02:42:16 +0800

    fix: train=false for running on test dataset

--- whiteboxBIM.py
+++ whiteboxBIM.py
@@ -19,12 +19,14 @@ from utils.lossmetrics import PixelAccuracy, DiceCoefficient
 import csv
 
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-ROOT_DIR = os.join.path(os.path.dirname(__file__), "data/MoNuSegTestData")
+ROOT_DIR = os.path.join(os.path.dirname(__file__), "data/MoNuSegTestData")
 BATCH_SIZE = 4
 NUM_WORKERS = 4
 PERSISTENT_WORKERS = True
 
 # Basic Iterative Method Attack
+
+
 def bim_attack(model, images, labels, epsilon, alpha, num_iterations):
     images.requires_grad = True
     original_images = images.clone().detach()
@@ -37,7 +39,7 @@ def bim_attack(model, images, labels, epsilon, alpha, num_iterations):
         loss = criterion(outputs, labels)
 
         model.zero_grad()
-        grad = autograd.grad(loss, images)[0]  
+        grad = autograd.grad(loss, images)[0]
 
         images = images + alpha * grad.sign()
         images = torch.clamp(images, 0, 1)
@@ -48,6 +50,8 @@ def bim_attack(model, images, labels, epsilon, alpha, num_iterations):
     return images
 
 # Plot images
+
+
 def show(imgs):
     if not isinstance(imgs, list):
         imgs = [imgs]
@@ -64,12 +68,13 @@ if __name__ == "__main__":
     test_transform = v2.Compose([
         v2.ToImage(),
         v2.ToDtype(torch.float32, scale=True),
-        v2.Resize(256, interpolation=v2.InterpolationMode.BILINEAR, antialias=True),
+        v2.Resize(256, interpolation=v2.InterpolationMode.BILINEAR,
+                  antialias=True),
         v2.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-    ])    
+    ])
 
     # Load test images into DataLoader
-    test_data = MoNuSegDataset(ROOT_DIR, transform=test_transform)
+    test_data = MoNuSegDataset(ROOT_DIR, transform=test_transform, train=False)
     test_dl = MultiEpochsDataLoader(test_data, batch_size=BATCH_SIZE,
                                     shuffle=False, num_workers=NUM_WORKERS,
                                     pin_memory=True, sampler=None,
@@ -104,17 +109,19 @@ if __name__ == "__main__":
     with open(csv_path, "w") as f:
         writer = csv.writer(f, delimiter=",")
         writer.writerow(["Fold", "Epsilon", "Alpha", "Image",
-                         "Original Pixel Mean", 
-                         "Original Dice Mean", 
+                         "Original Pixel Mean",
+                         "Original Dice Mean",
                          "Perturbed Pixel Mean",
                          "Perturbed Dice Mean"])
-    
+
     # Loading of the best model for each fold
     for k in tqdm(range(5)):
-        model = create_unet_model(resnet50, n_out=1, img_size=(256, 256), pretrained=True, weights=ResNet50_Weights.DEFAULT)
-        learn = Learner(dls=test_dl, model=model, metrics=fastai.metrics.Dice(), cbs=[MixedPrecision(),], loss_func=BCEWithLogitsLossFlat())
-        learn.load("fold_"+ str(k) +"_best")
-        print("Loaded model fold_"+ str(k) +"_best")
+        model = create_unet_model(resnet50, n_out=1, img_size=(
+            256, 256), pretrained=True, weights=ResNet50_Weights.DEFAULT)
+        learn = Learner(dls=test_dl, model=model, metrics=fastai.metrics.Dice(), cbs=[
+                        MixedPrecision(),], loss_func=BCEWithLogitsLossFlat())
+        learn.load("fold_" + str(k) + "_best")
+        print("Loaded model fold_" + str(k) + "_best")
 
         model.eval().to(DEVICE)
         preds, _ = learn.get_preds(dl=test_dl)
@@ -134,13 +141,17 @@ if __name__ == "__main__":
 
                 # Run the attack for each image
                 for j, (image, annotation) in enumerate(zip(images, annotations), 0):
-                    perturbed_image = bim_attack(model, image, annotation, epsilon, alpha, num_iterations)
-                    print("Epsilon value:", epsilon, "Alpha value:", alpha, "Image:", j)
-                    
+                    perturbed_image = bim_attack(
+                        model, image, annotation, epsilon, alpha, num_iterations)
+                    print("Epsilon value:", epsilon,
+                          "Alpha value:", alpha, "Image:", j)
+
                     # Save the perturbed image
-                    tensor_image = perturbed_image[0].cpu().detach().numpy().transpose(1, 2, 0)
+                    tensor_image = perturbed_image[0].cpu(
+                    ).detach().numpy().transpose(1, 2, 0)
                     tensor_image = np.clip(tensor_image, 0, 1)
-                    plt.imsave(os.path.join(path, "{}_{}_{}_{}.png".format(k,epsilon, alpha, j)), tensor_image)
+                    plt.imsave(os.path.join(path, "{}_{}_{}_{}.png".format(
+                        k, epsilon, alpha, j)), tensor_image)
 
                     # Evaluate the model on the perturbed image
                     perturbed_prediction = model(perturbed_image)
@@ -148,28 +159,34 @@ if __name__ == "__main__":
 
                     # Getting the original prediction based on index value
                     original_prediction = preds[j]
-                    
+
                     # Compute the pixel accuracy
-                    original_accuracy = PixelAccuracy(predictions=original_prediction, targets=annotation)
-                    perturbed_accuracy = PixelAccuracy(predictions=perturbed_prediction, targets=annotation)
+                    original_accuracy = PixelAccuracy(
+                        predictions=original_prediction, targets=annotation)
+                    perturbed_accuracy = PixelAccuracy(
+                        predictions=perturbed_prediction, targets=annotation)
 
                     original_total_acc.append(original_accuracy)
                     perturbed_total_acc.append(perturbed_accuracy)
 
-                    print("Original Accuracy: {:.2%}, Perturbed Accuracy: {:.2%}".format(original_accuracy, perturbed_accuracy))
+                    print("Original Accuracy: {:.2%}, Perturbed Accuracy: {:.2%}".format(
+                        original_accuracy, perturbed_accuracy))
 
                     # Compute the dice coefficient
-                    original_dice = DiceCoefficient(predictions=original_prediction, targets=annotation)
-                    perturbed_dice = DiceCoefficient(predictions=perturbed_prediction, targets=annotation)
+                    original_dice = DiceCoefficient(
+                        predictions=original_prediction, targets=annotation)
+                    perturbed_dice = DiceCoefficient(
+                        predictions=perturbed_prediction, targets=annotation)
 
                     original_dice_scores.append(original_dice)
                     perturbed_dice_scores.append(perturbed_dice)
 
-                    print("Original Dice: {:.2%}, Perturbed Dice: {:.2%}".format(original_dice, perturbed_dice))            
+                    print("Original Dice: {:.2%}, Perturbed Dice: {:.2%}".format(
+                        original_dice, perturbed_dice))
 
                 # Compute the mean and standard deviation of the pixel accuracy and dice coefficient
                 ori_pa_mean = sum(original_total_acc) / len(images)
-                
+
                 ori_dice_mean = sum(original_dice_scores) / len(images)
 
                 perb_pa_mean = sum(perturbed_total_acc) / len(images)
@@ -179,7 +196,6 @@ if __name__ == "__main__":
                 # Save the mean and standard deviation of the pixel accuracy and dice coefficient
                 with open(csv_path, "a") as f:
                     writer = csv.writer(f, delimiter=",")
-                    writer.writerow([k, epsilon, alpha, j, 
+                    writer.writerow([k, epsilon, alpha, j,
                                     ori_pa_mean, ori_dice_mean,
                                     perb_pa_mean, perb_dice_mean,])
-                    
